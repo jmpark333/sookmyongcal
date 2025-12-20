ba2d029f7190097756c657af6a05c18c69e612e4
Park James <rg327024@gmail.com>
2025-12-20 10:55:03 +0900

    Fix ZaiClient import path from zai._client

--- chatbot_server.py
+++ chatbot_server.py
@@ -4,7 +4,7 @@ import json
 import os
 from rag_system import get_rag_context
 import logging
-from zai import ZaiClient
+from zai._client import ZaiClient
 
 # 로깅 설정
 logging.basicConfig(level=logging.INFO)
